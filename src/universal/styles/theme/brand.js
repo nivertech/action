9bd81031fd2b60b5af3fd630d4ab220ae1ea24d7
Terry Acker <ackernaut@gmail.com>
2018-01-12 16:47:56 -0600

    brand palette exploration

--- src/universal/styles/theme/brand.js
+++ src/universal/styles/theme/brand.js
@@ -9,4 +9,30 @@ const brand = {
   darkCyan: '#098d8f' // Teal
 };
 
-export default brand;
+const brand2018a = {
+  slateGray: '#4C2882', // Dark, primary purple
+  greyBlue: '#7F56B2', // Mid, primary purple variation
+  darkishPink: '#FD6857', // Warm, secondary orange
+  eggShell: '#FFF1E7', // Cream
+  darkCyan: '#64B7C7' // Cool, secondary teal
+};
+
+const brand2018b = {
+  slateGray: '#14023D', // Dark, primary purple
+  greyBlue: '#4C2882', // Mid, primary purple variation
+  darkishPink: '#D64133', // Warm, secondary orange
+  eggShell: '#FFF1E7', // Cream
+  darkCyan: '#318299' // Cool, secondary teal
+};
+
+const brand2018c = {
+  slateGray: '#14023D', // Dark, primary purple
+  // slateGray: '#2F1461', // Dark, primary purple
+  greyBlue: '#4C2882', // Mid, primary purple variation
+  darkishPink: '#FD6857', // Warm, secondary orange
+  // eggShell: '#FFF1E7', // Cream
+  eggShell: '#FDEF57', // Cream
+  darkCyan: '#64B7C7' // Cool, secondary teal
+};
+
+export default brand2018c;
